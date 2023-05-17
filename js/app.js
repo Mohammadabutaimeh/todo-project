f722b7d50234b5daa642f9c427b7a5c177158987
Mohammadabutaimeh <hamota741994@gmail.com>
2023-05-17 08:07:07 +0000

    continue editting js file

--- js/app.js
+++ js/app.js
@@ -1,24 +1,22 @@
 
 let userName = prompt('Please enter your name');
-const userGender = prompt('Please enter your gender!\n(male or female)');
-
+let userGender = prompt('Please enter your gender!\n(male or female)').toLowerCase();
 switch(userGender){
     case 'male':
-        userAge = prompt(`Please enter your age Mr ${userName}!`)
+        userAge = prompt(`Please enter your age Mr ${userName}!`).parseInt();
         
         break;
     case 'female':
-        userAge = prompt(`Please enter your age Ms ${userName}!`)
+        userAge = prompt(`Please enter your age Ms ${userName}!`).parseInt();
        
         break;
     default:
-        userAge = prompt(`Please enter your age ${userName}!`)
+        userAge = prompt(`Please enter your age ${userName}!`).parseInt();
         
 }
 
-if (parseInt(userAge)<=0) {
-   alert(`The age ${userAge} is less than or equal to zero !! `); 
-   
+if (userAge<=0) {
+   alert(`The age ${userAge} is less than or equal to zero !! `);   
 }
 
 let confirmation=confirm(" if you want to skip the welcoming message please press ok");
@@ -41,66 +39,39 @@ if (confirmation==false) {
 
 const moreQuestions = confirm('Do you want to Answer more questions ?');
 
- const answers = [3];
-    if(moreQuestions==true){
-        let quistion1=prompt(`Do you know how to code?`)
-        switch (quistion1) {
-            case 'yes':
-                quistion1=`yes`
-                console.log(`yes`);
-                break;
-             case 'no':
-                quistion1=`no`
-                console.log(`no`);
-                break;
+ let answers = [3];
+
+ function checkAnswers(quistion1) {
+    switch (quistion1) {
+        case 'yes':
+            quistion1=`yes`
+            break;
+         case 'no':
+            quistion1=`no`
+            break;
+        default:
+            quistion1=`invalid`
+            break;
+    }
+    return (quistion1)
+ }
+    if(moreQuestions==true)
+    {
+        let quistion1=prompt(`Do you know how to code?`).toLowerCase();
+       
+        let quistion2=prompt(`Have you ever fallen asleep in class and got called out by a teacher for it?`).toLowerCase();
         
-            default:
-                quistion1=`invalid`
-                console.log(`invalid`);
-                break;
-        }
-        let quistion2=prompt(`Have you ever fallen asleep in class and got called out by a teacher for it?`)
-        switch (quistion2) {
-            case 'yes':
-                quistion2=`yes`
-                console.log(`yes`);
-               
-                break;
-             case 'no':
-                quistion2=`no`
-                console.log(`no`);
-                break;
-        
-            default:
-                quistion2=`invalid`
-                console.log(`invalid`);
-                break;
-        }
+        let quistion3=prompt(`Have you ever stolen a candy bar from a local corner store?`).toLowerCase();
+
+         answers=[checkAnswers(quistion1),checkAnswers(quistion2),checkAnswers(quistion3)]
       
-    
-        let quistion3=prompt(`Have you ever stolen a candy bar from a local corner store?`)
-        switch (quistion3) {
-            case 'yes':
-                quistion3=`yes`
-                console.log(`yes`);
-                
-                break;
-             case 'no':
-                quistion3=`no`
-                console.log(`no`);
-                break;
-        
-            default:
-                quistion3=`invalid`
-                console.log(`invalid`);
-                break;
-        }
-        let answers=[quistion1,quistion2,quistion3]
-      
-        console.log(answers);
       }
       
-    
+    for (let i = 0; i < answers.length; i++) {
+
+        console.log(answers[i]); 
+        
+    }
      
     
      
